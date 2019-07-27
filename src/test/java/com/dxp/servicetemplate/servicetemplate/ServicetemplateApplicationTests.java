454b9116fabb701c49d7c4fe16f54af2d49d27b8
Rakesh Kumar Thawait <rakeshkumar.thawait2@t-mobile.com>
2019-07-28 01:47:27 +0530

    first commit

---
+++ src/test/java/com/dxp/servicetemplate/servicetemplate/ServicetemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxp.servicetemplate.servicetemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicetemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
